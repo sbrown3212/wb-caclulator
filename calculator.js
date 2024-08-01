04e2d03e49d3ddb55d5f60004dc30ecadf57e151
Stephen Brown <stephen.jr36@gmail.com>
2024-08-01 14:25:11 -0600

    completed step 3

--- calculator.js
+++ calculator.js
@@ -1,25 +1,25 @@
 function add(num1, num2) {
-  return +num1 + +num2;
+  return num1 + num2;
 }
 
 function subtract(num1, num2) {
-  return +num1 - +num2;
+  return num1 - num2;
 }
 
 function multiply(num1, num2) {
-  return +num1 * +num2;
+  return num1 * num2;
 }
 
 function divide(num1, num2) {
-  return +num1 / +num2;
+  return num1 / num2;
 }
 
 function power(num1, num2) {
-  return +num1 ** +num2;
+  return num1 ** num2;
 }
 
 function mod(num1, num2) {
-  return +num1 % +num2;
+  return num1 % num2;
 }
 
 function sqrt(num1) {
@@ -27,7 +27,19 @@ function sqrt(num1) {
 }
 
 
-function calculate(expression) {}
+function calculate(expression) {
+  // Split expression into individual tokens
+  const tokens = expression.split(` `)
+
+  const operator = tokens[1];
+  const num1 = Number(tokens[0]);
+  const num2 = Number(tokens[2]);
+
+  if (Number.isNaN(num1) || Number.isNaN(num2)) {
+    alert(`Invalid number(s)`);
+    return;
+  }
+}
 
 /* **************** DO NOT EDIT THE CODE BELOW **************** */
 /* ************************************************************ */
