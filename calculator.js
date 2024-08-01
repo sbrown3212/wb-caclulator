34d281f9216246723d6235097dc6f2cd3232fcad
Stephen Brown <stephen.jr36@gmail.com>
2024-08-01 14:53:27 -0600

    completed step 5

--- calculator.js
+++ calculator.js
@@ -31,29 +31,66 @@ function calculate(expression) {
   // Split expression into individual tokens
   const tokens = expression.split(` `)
 
+  // Alert if too many or too few inputs
+  if (tokens.length < 2 || tokens.length > 3) {
+    alert(`Invalid expression. Please try again.`);
+    return;
+  }
+
+  // Initialize variables
   let operator;
   let num1;
   let num2;
-
+  
+  // Assign values to variables. Alert if variables are invalid.
   if (tokens.length === 3) {
     operator = tokens[1];
     num1 = Number(tokens[0]);
     num2 = Number(tokens[2]);
-
     if (Number.isNaN(num1) || Number.isNaN(num2)) {
-        alert(`Invalid number(s). Please try again.`);
-        return;
-      }
+      alert(`Invalid number(s). Please try again.`);
+      return;
     }
+  }
   if (tokens.length === 2) {
     operator = tokens[0];
     num1 = Number(tokens[1])
   }
-    if (Number.isNaN(num1)) {
-        alert(`Invalid number. Please try again.`);
-        return;
-      }
+  if (Number.isNaN(num1)) {
+    alert(`Invalid number. Please try again.`);
+    return;
+  }
 
+  const validOperators = [`+`, `-`, `*`, `/`, `**`, `%`, `sqrt`]
+  
+  // Alert if operator is invalid
+  if (!validOperators.includes(operator)) {
+    alert(`Invalid operator. Please try again.`);
+    return;
+  }
+
+  if (operator === `+`) {
+    return add(num1, num2);
+  }
+  if (operator === `-`) {
+    return subtract(num1, num2);
+  }
+  if (operator === `*`) {
+    return multiply(num1, num2);
+  }
+  if (operator === `/`) {
+    return divide(num1, num2);
+  }
+  if (operator === `**`) {
+    return power(num1, num2);
+  }
+  if (operator === `%`) {
+    return mod(num1, num2);
+  }
+  if (operator === `sqrt`) {
+    return sqrt(num1);
+  }
+  
 }
 
 /* **************** DO NOT EDIT THE CODE BELOW **************** */
